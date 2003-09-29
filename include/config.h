114808aeba186a7c328b9feb02950daba2f91618
parasyte <parasyte>
2003-09-29 02:21:53 +0000

    added files for project

---
+++ include/config.h
@@ -0,0 +1,11 @@
+/* include/config.h.  Generated automatically by configure.  */
+/* include/config.h.in.  Generated automatically from configure.ac by autoheader.  */
+
+/* Name of package */
+#define PACKAGE "mycid"
+
+/* Define if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Version number of package */
+#define VERSION "0.0.1"
